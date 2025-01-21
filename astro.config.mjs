acab5d7c2419bb8e24fcec48405e4585280ec6f3
Sewdohe <sewdohe@protonmail.com>
2025-01-21 17:19:26 -0500

    inital skeleton work done start working on server integration

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), tailwind(), partytown(), sitemap()]
+});

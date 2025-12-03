93609ce6cc869c7d35041b658fbf3bfe4b007255
Sewdohe <sewdohe@protonmail.com>
2025-12-02 22:25:00 -0500

    using expressive code for blog posts. Much nicer looking.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
+import expressiveCode from 'astro-expressive-code';
 
 import icon from 'astro-icon';
 
@@ -14,9 +15,26 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://divnectar.com',
-  integrations: [react({
-    experimentalReactChildren: true
-  }), tailwind(), partytown(), sitemap(), icon(), sitemap()],
+  integrations: [
+    expressiveCode({
+      themes: ['catppuccin-mocha', 'catppuccin-latte'],
+      defaultProps: {
+        wrap: true,
+        preserveIndent: true,
+      },
+      styleOverrides: {
+        borderRadius: '0.5rem',
+        borderWidth: '2px',
+      },
+    }),
+    react({
+      experimentalReactChildren: true
+    }),
+    tailwind(),
+    partytown(),
+    sitemap(),
+    icon()
+  ],
   vite: {
     ssr: {
       noExternal: ['@builder.io']
